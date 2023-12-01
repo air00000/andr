b4b168e9abb981856a093a578f305a1c5b483d9f
ildar <faq3002@gmail.com>
2023-12-01 07:04:56 +0300

    p-10 without ViewModel

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -3,55 +3,38 @@ package com.example.myapplication
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.viewModels
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.lifecycle.ViewModel
-import com.example.myapplication.ui.theme.MyApplicationTheme
+
 
 class MainActivity : ComponentActivity() {
-    val viewModel: MyViewModel by viewModels()
+    // val viewModel: MyViewModel1 by viewModels()
     override fun onCreate(savedInstanceState: Bundle?) {
-        val i=viewModel.i
         super.onCreate(savedInstanceState)
         setContent {
-            Screen(i)
+            Screen0()
         }
     }
 }
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
 
-@Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
-    MyApplicationTheme {
-        Greeting("Android")
+fun Screen0() {
+    var i by remember {
+        mutableStateOf(0)
     }
-}
-
-class MyViewModel : ViewModel() {
-    val i = mutableStateOf(0)
-}
-
-@Composable
-fun Screen(i: MutableState<Int>) {
     Text(
-        text = "Клики: ${ i.value}",
-        modifier = Modifier.clickable(onClick = { i.value++ } )
+        text = "Клики: $i",
+        modifier = Modifier.clickable(onClick = { i++ } )
     )
 }
+
+
